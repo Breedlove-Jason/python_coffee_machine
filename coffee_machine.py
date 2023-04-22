db34142a2424dedf1b43f453b88cadf4c886ccbd
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-22 15:32:43 -0700

    first commit, project requirements

---
+++ coffee_machine.py
@@ -0,0 +1,13 @@
+from menu import MENU
+# 3 Hot Flavors
+
+
+# Coins Operate
+
+# Print Report that details the current resources
+# Check resources sufficient to make drink order
+# Process Coins
+# Check transaction successful
+# Make Coffee (deduct resources)
+
+
