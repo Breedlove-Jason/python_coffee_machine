f9812a8617dc943a2e02007783090e704d93109b
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-23 10:55:40 -0700

    finished print_report and make_coffee functions

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,13 +1,40 @@
-from menu import MENU
+from menu import MENU, resources
+
+
 # 3 Hot Flavors
 
 
 # Coins Operate
 
-# Print Report that details the current resources
-# Check resources sufficient to make drink order
-# Process Coins
-# Check transaction successful
-# Make Coffee (deduct resources)
+def print_report():
+    """Print report of all coffee machine resources"""
+    for key, value in resources.items():
+        print(f"{key}: {value}")
 
 
+def make_coffee(drink_name):
+    """Deduct resources from coffee machine and check that there are enough resources to make the drink"""
+    for key, value in MENU[drink_name]["ingredients"].items():
+        resources[key] -= value
+        if resources[key] < 0:
+            print(f"Sorry there is not enough {key}.")
+            resources[key] += value
+            return
+
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
+
+
+print("Welcome to the Coffee Machine")
+print_report()
+
+# print("What would you like? (espresso/latte/cappuccino):")
+print("First Drink")
+make_coffee("latte")
+print("Second Drink")
+make_coffee("latte")
+print("Ending Resources")
+print_report()
+
+# TODO: 3. Process coins
+
+# TODO: 4. Check transaction successful
