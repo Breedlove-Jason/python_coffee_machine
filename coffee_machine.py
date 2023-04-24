226da417b93b7664df63590d7ab3f1139eff8ab6
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-24 13:37:50 -0700

    refactored code to match requirements

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,15 +1,15 @@
 from menu import MENU, resources
+from art import logo
+
+print(logo)
 
 
-# 3 Hot Flavors
-
-
-# Coins Operate
-
-def print_report():
+def print_report(total):
     """Print report of all coffee machine resources"""
-    for key, value in resources.items():
-        print(f"{key}: {value}")
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${total:.2f}")
 
 
 def make_coffee(drink_name):
@@ -24,29 +24,67 @@ def make_coffee(drink_name):
     print(f"Here is your {drink_name} ☕️. Enjoy!")
 
 
-print("Welcome to the Coffee Machine")
-print_report()
+def coins_operate():
+    """Process coins and return total"""
+    print("Please insert coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return round(total, 2)
 
+
+print("Welcome to the Coffee Machine")
+
+machine_total = 0
 coffee_loop = True
 while coffee_loop:
     if resources["water"] < 0 or resources["milk"] < 0 or resources["coffee"] < 0:
         print("Sorry, there is not enough resources to make another drink.")
         coffee_loop = False
         break
-    user_input = int(input("What would you like? (1. for espresso | 2. for latte | 3. for cappuccino):"))
-    if user_input == 1:
-        make_coffee("espresso")
-    elif user_input == 2:
-        make_coffee("latte")
-    elif user_input == 3:
-        make_coffee("cappuccino")
+
+    user_input = input("What would you like? (espresso = 'esp' | latte = 'lat' | cappuccino = 'cap' | report) ")
+
+    if user_input == 'esp':
+        money = coins_operate()
+        if money >= MENU["espresso"]["cost"]:
+            money -= MENU["espresso"]["cost"]
+            make_coffee("espresso")
+            machine_total += MENU["espresso"]["cost"]
+            print(f"Here is ${money:.2f} in change.")
+            continue
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+        break
+    elif user_input == 'lat':
+        money = coins_operate()
+        if money >= MENU["latte"]["cost"]:
+            money -= MENU["latte"]["cost"]
+            make_coffee("latte")
+            machine_total += MENU["latte"]["cost"]
+            print(f"Here is ${money:.2f} in change.")
+            continue
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            break
+    elif user_input == 'cap':
+        money = coins_operate()
+        if money >= MENU["cappuccino"]["cost"]:
+            money -= MENU["cappuccino"]["cost"]
+            make_coffee("cappuccino")
+            machine_total += MENU["cappuccino"]["cost"]
+            print(f"Here is ${money:.2f} in change.")
+            continue
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            break
+    elif user_input == 'report':
+        print_report(machine_total)
+        continue
+    elif user_input == 'off':
+        coffee_loop = False
+        break
     else:
         print("Invalid input")
-
-print("Ending Resources")
-print_report()
-
-
-# TODO: 3. Process coins
-
-# TODO: 4. Check transaction successful
+        continue
